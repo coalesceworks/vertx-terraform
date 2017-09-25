7be851540c00bdf564bcfb282a533d0aac5e369a
Sandeep Kotha <akotha31@gmail.com>
2017-09-25 13:45:20 +0100

    update vertx terraform deployments

--- variables.tf
+++ variables.tf
@@ -3,3 +3,4 @@ variable "secret_key" {}
 variable "region" {
   default = "us-east-1"
 }
+variable "aws_node_public_key" {}
--- vertx_env.tf
+++ vertx_env.tf
@@ -6,11 +6,25 @@ provider "aws" {
 
 
 resource "aws_instance" "vertx" {
-  ami           = "ami-b374d5a5"
+  ami           = "ami-3755bd4d"
   instance_type = "t2.micro"
+  key_name 		=	"vertx_rsa"
+}
+
+resource "aws_iam_user" "user" {
+  name = "ubuntu"
+  path = "/"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "vertx_rsa"
+  public_key = "${var.aws_node_public_key}"
 }
 
 resource "aws_eip" "ip" {
   instance = "${aws_instance.vertx.id}"
 }
 
+output "ip" {
+  value = "${aws_eip.ip.public_ip}"
+}
